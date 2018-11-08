554547ac848656c6897e8d7fbab62263a3306e61
Tobias Jensen <tobiasjensen@Tobiass-MacBook-Pro.local>
2018-11-08 15:42:41 +0100

    OrderCache+Config

--- src/main/java/cache/ProductCache.java
+++ src/main/java/cache/ProductCache.java
@@ -22,7 +22,7 @@ public class ProductCache {
 
   public ArrayList<Product> getProducts(Boolean forceUpdate) {
 
-    // If we whis to clear cache, we can set force update.
+    // If we wish to clear cache, we can set force update.
     // Otherwise we look at the age of the cache and figure out if we should update.
     // If the list is empty we also check for new products
     if (forceUpdate
